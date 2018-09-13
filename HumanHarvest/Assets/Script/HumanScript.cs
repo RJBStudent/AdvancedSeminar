c7af9da0f30748ad8e5f29665f50d0dc4b8f76a6
361rjb <361rjb@gmail.com>
2018-09-12 21:14:52 -0400

    Humans Movement and Trashcans
    Trashcans exist around the map. Humans each have a list of trashcans and will look for the closest empty trashcan. Each human phases through walls. Player can interact with trashcan instead of humans now. And carry the human to the ufo spot.

---
+++ HumanHarvest/Assets/Script/HumanScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanScript : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    public GameObject[] trashcanList;
+    public GameObject currentTrashTarget;
+
+    
+
+    Vector2 targetDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        trashcanList = GameObject.FindGameObjectsWithTag("Trashcan");
+        UpdateClosest();
+    }
+
+    void UpdateClosest()
+    {
+        float shortestDistance = 200f;
+        foreach (GameObject trashcan in trashcanList)
+        {
+            if (!trashcan.GetComponent<TrashcanScript>().HasHuman)
+            {
+                float tempDistance = Vector2.Distance(trashcan.transform.position, gameObject.transform.position);
+                tempDistance = Mathf.Abs(tempDistance);
+                if (tempDistance < shortestDistance)
+                {
+                    shortestDistance = tempDistance;
+                    currentTrashTarget = trashcan;
+                }
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateClosest();
+        Move();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Trashcan")
+        {
+           
+            if (!col.gameObject.GetComponent<TrashcanScript>().HasHuman)
+            {
+                Debug.Log("EnteredTrash");
+                col.gameObject.GetComponent<TrashcanScript>().HasHuman = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void Move()
+    {
+        targetDirection = currentTrashTarget.transform.position - transform.position;
+        targetDirection.Normalize();
+
+        targetDirection /= speed;
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x+ targetDirection.x, gameObject.transform.position.y + targetDirection.y, gameObject.transform.position.z);
+    }
+
+}
